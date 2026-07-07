d4e324c4ae25141b7b761729037a89767d4678f3
Abhishek98negi <nabhishek971@gmail.com>
2026-07-07 17:10:45 +0530

    pushing code to github

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Heart-disease-prediction-system
+
+![alt text](image-1.png)
+![alt text](image-2.png)
---
+++ frontend/app.py
@@ -0,0 +1,98 @@
+import os
+import requests
+import streamlit as st
+from dotenv import load_dotenv
+
+# load .env to env vars
+load_dotenv()
+
+API_URL = os.getenv("API_URL")
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background-color: #FAACBF;  /* Replace with your desired color */
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+st.set_page_config(
+    page_title="Heart Disease Prediction System",
+    page_icon="🧡",
+    layout="centered"
+)
+
+st.title("🧡Heart Disease Prediction System")
+st.write("An ML model to predict heart disease risk.")
+
+st.subheader("Enter patient details and click **Predict**")
+
+col1, col2, col3, col4 = st.columns(4)
+
+with col1:
+    age = st.number_input("Age", 1, 120, 52)
+    sex = st.selectbox("Sex (1 = Male, 0 = Female)", [0, 1])
+    cp = st.number_input("Chest Pain Type (cp)", 0, 3, 0)
+    trestbps = st.number_input("Resting Blood Pressure", 0, 250, 125)
+
+
+with col2:
+    fbs = st.selectbox("Fasting Blood Sugar > 120 mg/dl", [0, 1])
+    restecg = st.number_input("Resting ECG (restecg)", 0, 2, 1)
+    thalach = st.number_input("Max Heart Rate (thalach)", 0, 250, 168)
+
+
+with col3:
+    oldpeak = st.number_input("Oldpeak (ST depression)", 0.0, 10.0, 1.0)
+    slope = st.number_input("Slope", 0, 2, 2)
+    ca = st.number_input("Number of Major Vessels (ca)", 0, 4, 0)
+
+with col4:
+    chol = st.number_input("Cholesterol", 0, 600, 212)
+    exang = st.selectbox("Exercise Induced Angina", [0, 1])
+    thal = st.number_input("Thal", 0, 3, 2)
+
+
+if st.button("🔍 Predict"):
+    input_data = {
+        "age": age,
+        "sex": sex,
+        "cp": cp,
+        "trestbps": trestbps,
+        "chol": chol,
+        "fbs": fbs,
+        "restecg": restecg,
+        "thalach": thalach,
+        "exang": exang,
+        "oldpeak": oldpeak,
+        "slope": slope,
+        "ca": ca,
+        "thal": thal
+    }
+
+    response = requests.post(API_URL, json=input_data)
+
+    if response.status_code != 200:
+        st.error("Something went wrong. Try again later...")
+    
+    else:
+        result = response.json()
+        prediction = result["prediction"]
+        probability = result["probability"]
+        diagnosis = result["diagnosis"]
+
+        st.divider()
+
+        st.metric(
+            label="Heart Disease Probability",
+            value=f"{probability:.2f}"
+        )
+
+        if prediction == 1:
+            st.error(f"⚠️Model Prediction: {diagnosis}")
+        else:
+            st.success(f"✅ Model Prediction: {diagnosis}")
+
+# streamlit run frontend/app.py
